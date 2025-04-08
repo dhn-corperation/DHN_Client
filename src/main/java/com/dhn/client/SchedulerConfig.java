a68fcb212afc77e0e2fbf83e2f8a175358a2fce5
mango7431 <rsm5065@naver.com>
2025-04-08 09:30:02 +0900

    Scheduler pool size 30 -> 10

--- src/main/java/com/dhn/client/SchedulerConfig.java
+++ src/main/java/com/dhn/client/SchedulerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
-	private final int POOL_SIZE = 30;
+	private final int POOL_SIZE = 10;
 	
 	@Override
 	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
