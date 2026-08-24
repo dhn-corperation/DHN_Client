4050f486d35e10f8307ebd7a7932792301d6c5f6
mango7431 <rsm5065@naver.com>
2026-08-24 11:09:32 +0900

    CMS end

--- src/main/java/com/dhn/client/service/RequestService.java
+++ src/main/java/com/dhn/client/service/RequestService.java
@@ -10,8 +10,6 @@ public interface RequestService {
     void updateSendComplete(SQLParameter param) throws Exception;
     void updateInvalidData(List<String> invalidList, Msg_Log ml) throws Exception;
 
-    // 테이블 존재 체크용 (만약 필요하다면)
-    void logTableCheck(String msgTable, String logTable) throws Exception;
     // 결과 처리 프로세스 (Update -> Insert -> Delete)
     void applyResultProcess(Msg_Log ml) throws Exception;
 }
