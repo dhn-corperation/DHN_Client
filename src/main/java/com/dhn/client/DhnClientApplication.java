36f40f5a346356850618f1f968e8e4ad4ab15e40
mango7431 <rsm5065@naver.com>
2026-09-03 17:24:01 +0900

    main context add

--- src/main/java/com/dhn/client/DhnClientApplication.java
+++ src/main/java/com/dhn/client/DhnClientApplication.java
@@ -3,6 +3,7 @@ package com.dhn.client;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,8 +12,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class DhnClientApplication {
 
+	private static ConfigurableApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(DhnClientApplication.class, args);
+//		SpringApplication.run(DhnClientApplication.class, args);
+		context = SpringApplication.run(DhnClientApplication.class, args);
+	}
+
+	public static void stop() {
+		if (context != null) {
+			context.close();
+		}
 	}
 
 }
